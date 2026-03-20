baed01f964369f5542b4ab3e72f5943130c00989
satelitlima00 <128956610+satelitlima00@users.noreply.github.com>
2026-03-20 16:30:22 +0700

    Update vm.sh

--- vm.sh
+++ vm.sh
@@ -196,16 +196,16 @@ create_new_vm() {
     done
 
     while true; do
-        read -p "$(print_status "INPUT" "Enter username (default: ubuntu): ")" USERNAME
-        USERNAME="${USERNAME:-ubuntu}"
+        read -p "$(print_status "INPUT" "Enter username (default: suto): ")" USERNAME
+        USERNAME="${USERNAME:-suto}"
         if validate_input "username" "$USERNAME"; then
             break
         fi
     done
 
     while true; do
-        read -s -p "$(print_status "INPUT" "Enter password (default: ubuntu): ")" PASSWORD
-        PASSWORD="${PASSWORD:-ubuntu}"
+        read -s -p "$(print_status "INPUT" "Enter password (default: suto): ")" PASSWORD
+        PASSWORD="${PASSWORD:-suto}"
         echo
         if [ -n "$PASSWORD" ]; then
             break
@@ -215,8 +215,8 @@ create_new_vm() {
     done
 
     while true; do
-        read -p "$(print_status "INPUT" "Disk size (default: 90G): ")" DISK_SIZE
-        DISK_SIZE="${DISK_SIZE:-90G}"
+        read -p "$(print_status "INPUT" "Disk size (default: 100G): ")" DISK_SIZE
+        DISK_SIZE="${DISK_SIZE:-100G}"
         if validate_input "size" "$DISK_SIZE"; then
             break
         fi
