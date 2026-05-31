d9909eab9c92990db19c9c92c04719621e111ab6
satelitlima00 <128956610+satelitlima00@users.noreply.github.com>
2026-05-31 21:06:33 +0700

    Update vm.sh

--- vm.sh
+++ vm.sh
@@ -231,8 +231,8 @@ create_new_vm() {
     done
 
     while true; do
-        read -p "$(print_status "INPUT" "Number of CPUs (default: 2): ")" CPUS
-        CPUS="${CPUS:-2}"
+        read -p "$(print_status "INPUT" "Number of CPUs (default: 3): ")" CPUS
+        CPUS="${CPUS:-3}"
         if validate_input "number" "$CPUS"; then
             break
         fi
